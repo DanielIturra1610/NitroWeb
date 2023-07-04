b42a27270108c1475362ab138fdef6917a3d8cbc
Daniel <daniel.eduardo1610@gmail.com>
2023-07-04 19:00:32 -0400

    Se realizan cambios al html, css y base de datos

--- nitroweb/urls.py
+++ nitroweb/urls.py
@@ -1,6 +1,27 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = {
     path('index', views.index, name='index'),
+    path('classicgames', views.classicgames, name='classicgames'),
+    path('classicgames2', views.classicgames2, name='classicgames2'),
+    path('contactanos', views.contactanos, name='contactanos'),
+    path('iniciarsesion', views.iniciarsesion, name='iniciarsesion'),
+    path('newgames', views.newgames, name='newgames'),
+    path('newgames2', views.newgames2, name='newgames2'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('registrarse', views.registrarse, name='registrarse'),
+    path('retrogames', views.retrogames, name='retrogames'),
+    path('retrogames2', views.retrogames2, name='retrogames2'),
+    path('games', views.games, name='games'),
+    path('crud', views.crud, name='crud'),
+    path('agregar/', views.agregar, name='agregar'),
+    path('eliminar/<codigo>', views.eliminar, name='eliminar'),
+    path('editar/<codigo>', views.editar, name='editar'),
+    path('editarJuego/', views.editarJuego)     
+
+    
+
+    
 }
--- nitroweb/views.py
+++ nitroweb/views.py
@@ -1,7 +1,103 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Juego,Categoria
+from django.contrib import messages
+
 
 # Create your views here.
 
 def index(request):
     context={}
     return render(request, 'nitroweb/index.html', context)
+
+def nosotros(requets):
+    context={}
+    return render(request, 'nitroweb/Nosotros.html', context)
+
+def games(request):
+    context={}
+    return render(request, 'nitroweb/games.html', context)
+
+def iniciarsesion(request):
+    context={}
+    return render(request, 'nitroweb/IniciarSesion.html', context)
+
+def classicgames(request):
+    context={}
+    return render(request, 'nitroweb/ClassicGames.html', context)
+
+def contactanos(request):
+    context={}
+    return render(request, 'nitroweb/Contactanos.html', context)
+
+def newgames(request):
+    context={}
+    return render(request, 'nitroweb/NewGames.html', context)
+
+def newgames2(request):
+    context={}
+    return render(request, 'nitroweb/NewGames2.html', context)
+
+def classicgames2(request):
+    context={}
+    return render(request, 'nitroweb/ClassicGames2.html', context)
+
+def retrogames(request):
+    context={}
+    return render(request, 'nitroweb/RetroGames.html', context)
+
+def retrogames2(request):
+    context={}
+    return render(request, 'nitroweb/RetroGames2.html', context)
+
+def registrarse(request):
+    context={}
+    return render(request, 'nitroweb/Registrarse.html', context)
+
+def satisf(request):
+    context={}
+    return render(request, 'nitroweb/Satisf.html', context)
+
+def crud(request):
+    juegos = Juego.objects.all()
+    categorias = Categoria.objects.all()
+    context={"juegos":juegos,"categorias":categorias}
+    return render(request, 'main/crud.html', context)
+
+def agregar(request):
+    nombre = request.POST['nombre']
+    precio = request.POST['precio']
+    categoria = request.POST['categoria']
+
+    objCategoria = Categoria.objects.get(id = categoria)
+    objJuego = Juego.objects.create(nombre=nombre, categoria=objCategoria, precio=precio)
+    messages.success(request, 'Juego Agregado')
+    return redirect('crud')
+
+def eliminar(request, codigo):
+    juego = Juego.objects.get(id=codigo)
+    juego.delete()
+    messages.success(request, 'Juego Eliminado')
+    return redirect('crud')
+
+def editar(request, codigo):
+    juego = Juego.objects.get(id=codigo)
+    categorias = Categoria.objects.all()
+    context={"juego":juego,"categorias":categorias}
+    return render(request, "main/editar.html", context)
+
+def editarJuego(request):
+    codigo = request.POST['id']
+    nombre = request.POST['nombre']
+    precio = request.POST['precio']
+    categoria = request.POST['categoria']
+
+    objCategoria = Categoria.objects.get(id = categoria)
+    objJuego = Juego.objects.get(id=codigo)
+    
+    objJuego.nombre = nombre
+    objJuego.categoria = objCategoria
+    objJuego.precio = precio
+    objJuego.save()
+
+    messages.success(request, 'Juego actualizado')
+    return redirect('crud') 
